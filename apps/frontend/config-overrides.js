42cd75bef0d1b75d9d8814c07d153f2492a78b68
Srinivas Akella <akellasrinivas9944@gmail.com>
2026-08-01 19:39:02 +0530

    husky changes and upgraded alll the packages

--- apps/frontend/config-overrides.js
+++ apps/frontend/config-overrides.js
@@ -1,6 +1,5 @@
 const { override, addBabelPlugins } = require('customize-cra');
 
-// override
 module.exports = {
   webpack: override(
     // customize-cra plugins here
@@ -9,4 +8,17 @@ module.exports = {
       return config;
     },
   ),
+  jest: config => {
+    // 1. Tell Jest to transpile modern ES modules from these specific node_modules
+    config.transformIgnorePatterns = [
+      '[/\\\\]node_modules[/\\\\](?!(@standard-schema|@reduxjs|react-router-dom|react-router|@remix-run)).+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+      '^.+\\.module\\.(css|sass|scss)$',
+    ];
+    // 2. Fix the missing exports map resolution in Jest 27 for react-router-dom
+    config.moduleNameMapper = {
+      ...config.moduleNameMapper,
+      '^react-router/dom$': require.resolve('react-router-dom'),
+    };
+    return config;
+  },
 };
