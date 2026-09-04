96c0f9e1b8a1f72595749c66b6e21810f11a79ae
Srinivas Akella <akellasrinivas9944@gmail.com>
2026-09-04 13:38:15 +0530

    added health route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://todo-qa-monorepo-frontend-one.vercel.app/",
+      "https://todo-qa-monorepo-frontend-one.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
